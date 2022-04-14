03eaacfaa771a60452626b8ccaae3a18925e913a
Mitchell Kember <mk12360@gmail.com>
2022-04-14 00:44:58 -0400

    Done

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 Web Math Demo is a web app that compares ways of rendering math notation. This is useful if you're trying to choose a strategy for your blog or website.
 
-It supports the following:
-
-- **Input**: [AsciiMath], [TeX], [MathML]
-- **Output**: [MathML] (native and polyfilled), [KaTeX], [MathJax]
+It supports [AsciiMath], [TeX], and [MathML] input, and renders to [MathJax] HTML+CSS, [KaTeX] HTML+CSS, and using the browser's native [MathML] rendering. As of 2022, Firefox has the best MathML support, Safari has lower quality support, and Chrome has none at all.
 
 ## Usage
 
 Clone and open index.html, or visit https://mk12.github.io/web-math-demo.
+
+As you type in the input boxes, the app automatically updates other boxes, or marks them stale (indicated with a red background) if it cannot. For example, typing in AsciiMath will update everything, but typing in TeX will make AsciiMath stale because there is no reverse transformation. The conversion from TeX to MathML can be done via MathJax or KaTeX, controlled by radio buttons. If you type in the MathML box, it will show the rendered MathML and also MathJax HTML+CSS, but not KaTeX HTML+CSS because KaTeX does not support MathML as input (so unlike MathJax, it cannot act as a MathML polyfill).
 
 ## License
 
@@ -18,7 +17,7 @@ Clone and open index.html, or visit https://mk12.github.io/web-math-demo.
 Web Math Demo is available under the MIT License; see [LICENSE](LICENSE.md) for details.
 
 [AsciiMath]: http://asciimath.org
-[MathML]: https://www.w3.org/Math
-[KaTeX]: https://katex.org
-[MathJax]: https://www.mathjax.org
 [TeX]: https://www.latex-project.org
+[MathML]: https://www.w3.org/Math
+[MathJax]: https://www.mathjax.org
+[KaTeX]: https://katex.org
--- script.js
+++ script.js
@@ -1,44 +1,230 @@
 // Copyright 2022 Mitchell Kember. Subject to the MIT License.
 
-const $asciimath = document.getElementById("Asciimath");
-const $tex = document.getElementById("Tex");
-const $mathml = document.getElementById("Mathml");
-const $mathjax = document.getElementById("Mathjax");
-const $katex = document.getElementById("Katex");
-const $mathmlBrowser = document.getElementById("MathmlBrowser");
-const $mathmlCss = document.getElementById("MathmlCss");
-const $mathmlMathjax = document.getElementById("MathmlMathjax");
+window.addEventListener("load", () => {
+  // Describes which cards get derived from which other cards, by HTML ID.
+  const derivations = {
+    asciimath: [],
+    tex: ["asciimath"],
+    mathml: ["tex"],
+    mathjaxHtmlCss: ["tex"],
+    katexHtmlCss: ["tex"],
+    nativeMathml: ["mathml"],
+  };
 
-const asciiMathParser = new AsciiMathParser();
+  const graph = {};
+  let lastUpdateFn = () => {};
 
-function stale(textarea) {
-    textarea.classList.add("stale");
-}
+  // Populate nodes in the card graph.
+  for (const [id, incoming] of Object.entries(derivations)) {
+    const element = document.getElementById(id);
+    let card = element;
+    while (!card.classList.contains("card")) {
+      card = card.parentElement;
+    }
+    const accessors = {};
+    if (element.tagName === "TEXTAREA") {
+      accessors.get = () => element.value;
+      accessors.set = (value) => (element.value = value);
+    } else {
+      accessors.get = () => element.innerHTML;
+      accessors.setHTML = (html) => (element.innerHTML = html);
+      accessors.setChild = (child) => {
+        while (element.firstChild) {
+          element.removeChild(element.firstChild);
+        }
+        element.appendChild(child);
+      };
+    }
+    graph[id] = {
+      id,
+      element,
+      card,
+      incoming,
+      outgoing: [],
+      ...accessors,
+    };
+  }
 
-function fresh(textarea) {
-    textarea.classList.remove("stale");
-}
+  // Populate outgoing edges in the card graph.
+  for (const [id, incoming] of Object.entries(derivations)) {
+    for (const other of incoming) {
+      graph[other].outgoing.push(id);
+    }
+  }
 
-$asciimath.oninput = function() {
-    fresh(this);
-    $tex.value = asciiMathParser.parse(this.value);
-    const node = parseMath(this.value);
-    console.log(node);
-    // while ($mathml.firstChild) {
-    //     $mathml.removeChild($mathml.firstChild);
-    // }
-    // $mathml.appendChild(asciimath.parseMath(this.value));
-};
+  // Depth-first search helper.
+  function dfs(x, prop, f) {
+    for (const id of x[prop]) {
+      const y = graph[id];
+      f(x, y);
+      dfs(y, prop, f);
+    }
+  }
 
-$tex.oninput = function() {
-    fresh(this);
-    stale($asciimath);
-};
+  // Helpers for marking cards as stale.
+  function markStale(node) {
+    node.card.classList.add("card--stale");
+  }
+  function markFresh(node) {
+    node.card.classList.remove("card--stale");
+  }
 
-$mathml.oninput = function() {
-    fresh(this);
-    stale($asciimath);
-    stale($tex);
-}
+  // Add event listeners for reactive updates.
+  for (const node of Object.values(graph)) {
+    if (!node.outgoing) {
+      continue;
+    }
+    node.update = () => {
+      dfs(node, "incoming", (_, y) => markStale(y));
+      markFresh(node);
+      dfs(node, "outgoing", (x, y) => {
+        markFresh(y);
+        propagate(x, y);
+      });
+      lastUpdateFn = node.update;
+    };
+    node.element.addEventListener("input", () => node.update());
+  }
 
-$asciimath.oninput();
+  const asciiMathParser = new AsciiMathParser();
+
+  // Set up checkbox for toggling display math mode.
+  const displayModeCheckbox = document.getElementById("displayMode");
+  displayModeCheckbox.addEventListener("input", () => lastUpdateFn());
+
+  // Set up radio buttons for controlling the TeX to MathML conversion.
+  const mathmlRadioButtons = Array.from(
+    document.querySelectorAll('input[name="mathmlGenerator"]')
+  );
+  let activeMathmlRadioButtton = mathmlRadioButtons.find((b) => b.checked);
+  for (const button of mathmlRadioButtons) {
+    button.addEventListener("input", () => {
+      activeMathmlRadioButtton = mathmlRadioButtons.find((b) => b.checked);
+      if (lastUpdateFn == graph.asciimath.update) {
+        lastUpdateFn();
+      } else {
+        graph.tex.update();
+      }
+    });
+  }
+
+  // Returns the MathML generator to use ("mathjax" or "katex").
+  function mathmlGenerator() {
+    activeMathmlRadioButtton.checked = true;
+    return activeMathmlRadioButtton.value;
+  }
+
+  // Special case for MathML converting to HTML+CSS.
+  const updateNativeMathml = graph.mathml.update;
+  graph.mathml.update = () => {
+    activeMathmlRadioButtton.checked = false;
+    markStale(graph.katexHtmlCss);
+    updateNativeMathml();
+    propagate(graph.mathml, graph.mathjaxHtmlCss);
+  };
+
+  // Add a <style> tag for injecting MathJax styles.
+  const mathjaxStyle = document.createElement("style");
+  document.head.appendChild(mathjaxStyle);
+
+  // Returns options to use for MathJax rendering in the given graph node.
+  function mathjaxOptions(node) {
+    return {
+      ...MathJax.getMetricsFor(node.element),
+      display: displayModeCheckbox.checked,
+    };
+  }
+
+  // Returns options to use for KaTeX rendering to the given output format.
+  function katexOptions(output) {
+    return {
+      output,
+      displayMode: displayModeCheckbox.checked,
+      throwOnError: false,
+    };
+  }
+
+  // Updates graph node y based on graph node x.
+  function propagate(x, y) {
+    switch (x.id) {
+      case "asciimath":
+        switch (y.id) {
+          case "tex":
+            y.set(asciiMathParser.parse(x.get()));
+            break;
+        }
+        break;
+      case "tex":
+        switch (y.id) {
+          case "mathml":
+            switch (mathmlGenerator()) {
+              case "mathjax":
+                y.set(MathJax.tex2mml(x.get(), mathjaxOptions(y)));
+                break;
+              case "katex":
+                y.set(
+                  katex
+                    .renderToString(x.get(), katexOptions("mathml"))
+                    .replace(/^<span class="katex">/, "")
+                    .replace(/<\/span>$/, "")
+                );
+                break;
+            }
+            break;
+          case "mathjaxHtmlCss":
+            y.setChild(MathJax.tex2chtml(x.get(), mathjaxOptions(y)));
+            mathjaxStyle.replaceWith(MathJax.chtmlStylesheet());
+            break;
+          case "katexHtmlCss":
+            katex.render(x.get(), y.element, katexOptions("html"));
+            break;
+        }
+        break;
+      case "mathml":
+        switch (y.id) {
+          case "mathjaxHtmlCss":
+            y.setChild(MathJax.mathml2chtml(x.get(), mathjaxOptions(y)));
+            mathjaxStyle.replaceWith(MathJax.chtmlStylesheet());
+            break;
+          case "nativeMathml":
+            y.setHTML(x.get());
+            break;
+        }
+        break;
+    }
+  }
+
+  // Set up slider to change the amount of space in cards.
+  const spaceStyle = document.createElement("style");
+  document.head.appendChild(spaceStyle);
+  document.getElementById("spaceSlider").addEventListener("input", function () {
+    const value = 300 + 100 * this.value;
+    spaceStyle.innerHTML = `
+      .card {
+        min-width: ${value}px;
+        min-height: ${value}px;
+      }
+    `;
+  });
+
+  // Set up slider to change the size of rendered math.
+  const sizeStyle = document.createElement("style");
+  document.head.appendChild(sizeStyle);
+  document.getElementById("sizeSlider").addEventListener("input", function () {
+    const value = 1.5 + 0.5 * this.value;
+    sizeStyle.innerHTML = `
+      div.card-value {
+        font-size: ${value}em;
+      }
+    `;
+  });
+
+  // Allow cards to be dragged around.
+  Sortable.create(document.querySelector(".card-grid"), {
+    animation: 200,
+    handle: ".card-handle",
+  });
+
+  // Update cards based on the default AsciiMath content in index.html.
+  graph.asciimath.update();
+});
